112fe6a37139151e46c11a065a51b2f724318fd0
ALI <ali.ranjbar440.004@gmail.com>
2024-08-12 02:10:52 -0700

    Add files via upload

---
+++ PythonApplication20.py
@@ -0,0 +1,17 @@
+Fruits={"apple","mango","berry","orange","cherry","peach","orange","orange"}
+
+Fruits.add("melon")
+
+result=Fruits.copy()
+print(result)
+
+
+print(Fruits)
+
+newlist=list(Fruits)
+newlist.pop()
+newlist[1]="Benana"
+print(newlist)
+newset=set(newlist)
+print(newset)
+
